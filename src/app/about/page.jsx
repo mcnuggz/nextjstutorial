a3c1c40b4c508346009d3886197776b2c5022544
Ryan Webb <dzanfox@gmail.com>
2024-07-27 21:45:55 -0500

    responsive navbar, external img configuration and images added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-    return <div>About Page</div>;
+    return (
+        <div>
+            <div className={styles.imgContainer}>
+                <Image
+                    src="https://images.pexels.com/photos/26861781/pexels-photo-26861781/free-photo-of-a-person-paragliding-in-the-sky-with-an-orange-parachute.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt=""
+                    fill
+                />
+            </div>
+        </div>
+    );
 };
 
 export default AboutPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,29 +1,69 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+
+const links = [
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "About",
+        path: "/about",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+    },
+    {
+        title: "Blog",
+        path: "/blog",
+    },
+];
+
 const Links = () => {
-    const links = [
-        {
-            title: "Home",
-            path: "/",
-        },
-        {
-            title: "About",
-            path: "/about",
-        },
-        {
-            title: "Contact",
-            path: "/contact",
-        },
-        {
-            title: "Blog",
-            path: "/blog",
-        },
-    ];
+    const [open, setOpen] = useState(false);
+
+    // Temporary
+    const session = true;
+    const isAdmin = true;
     return (
-        <div className={styles.links}>
-            {links.map((link) => (
-                <NavLink item={link} key={link.title}></NavLink>
-            ))}
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <NavLink item={link} key={link.title}></NavLink>
+                ))}
+                {session ? (
+                    <>
+                        {isAdmin && (
+                            <NavLink
+                                item={{ title: "Admin", path: "/admin" }}
+                            />
+                        )}
+                        <button className={styles.logoutBtn}>Log Out</button>
+                    </>
+                ) : (
+                    <NavLink item={{ title: "Login", path: "/login" }} />
+                )}
+            </div>
+            {/*     
+                sets state variable (prev) to its opposite value
+                i.e (false set to true) or (true set to false)
+            */}
+            <button
+                onClick={() => setOpen((prev) => !prev)}
+                className={styles.menuBtn}>
+                Menu
+            </button>
+            {open && (
+                <div className={styles.mobileLinks}>
+                    {links.map((link) => (
+                        <NavLink item={link} key={link.title} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
